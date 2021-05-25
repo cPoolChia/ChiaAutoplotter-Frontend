62330c92334cf7e02aafef0de7704b3fac8aae78
Павел Жуков <33721692+LeaveMyYard@users.noreply.github.com>
2021-05-25 15:47:08 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 export const config = {
-  serverUrl:
-    process.env.NODE_ENV !== "production"
-      ? "http://127.0.0.1:8000"
-      : "https://api.cpool.farm",
-  websocketUrl:
-    process.env.NODE_ENV !== "production"
-      ? "ws://127.0.0.1:8000"
-      : "wss://api.cpool.farm",
+    serverUrl:
+        process.env.NODE_ENV !== "production"
+            ? "http://127.0.0.1:8000"
+            : "https://admin.cpool.farm/api",
+    websocketUrl:
+        process.env.NODE_ENV !== "production"
+            ? "ws://127.0.0.1:8000"
+            : "wss://admin.cpool.farm/api",
 };
